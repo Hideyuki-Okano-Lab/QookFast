b756bd9325a9a03d306437c4b1a37091c367ec2f
yo-aka-gene <yujiokano@keio.jp>
2026-04-24 06:01:29 +0900

    :pencil2: fix typo

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,7 +7,7 @@ except ImportError:
     print(" [ERROR] 'jinja2-time' is not installed!")
     print("   QookFast template requires the 'jinja2-time' extension.")
     print("   Please install it before running Cookiecutter:")
-    print("\n       pip install jinja2-time\n")
+    print("\n   pip install jinja2-time\n")
     print("   Then, try running Cookiecutter again.")
     print("!" * 60 + "\n")
 
